e7fe8a87f0fa66502c133fdb79cd7f97a4344a8e
Brian Ketelsen <mail@bjk.fyi>
2020-04-21 16:18:38 -0400

    first post

---
+++ src/components/card.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Card = ({ article }) => {
+  return (
+    <Link to={`/article/${article.node.slug}`} className="uk-link-reset">
+      <div className="uk-card uk-card-muted">
+        <div className="uk-card-media-top">
+          <img
+            src={article.node.image.publicURL}
+            alt={article.node.image.publicURL}
+            height="100"
+          />
+        </div>
+        <div className="uk-card-body">
+
+              <p id="category" className="uk-text-uppercase">
+          {article.node.categories.map((cat, i) => {
+            return (
+                <span key={`category-${cat.name}`} className="categories">{cat.display}</span>
+              )
+          })}
+          </p>
+          <p id="title" className="uk-text-large">
+            {article.node.title}
+          </p>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default Card
